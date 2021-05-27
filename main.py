9d5440bb7be82d8fe1d4d9bf2b8a42878bd37809
Marios1989 <68182435+Marios1989@users.noreply.github.com>
2021-05-27 15:38:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,13 @@ while cap.isOpened():
     # define range of blue color in HSV
     lower_blue = np.array([110, 50, 50])
     upper_blue = np.array([130, 255, 255])
+    
+    lower_green = np.array([50, 50, 50])
+    upper_green = np.array([70, 255, 255])
 
     # Threshold the HSV image to get only blue colors
     mask = cv.inRange(hsv, lower_blue, upper_blue)
+    mask_green = cv.inRange(hsv, lower_green, upper_green)
 
     # Bitwise-AND mask and original image
     res = cv.bitwise_and(frame, frame, mask=mask)
